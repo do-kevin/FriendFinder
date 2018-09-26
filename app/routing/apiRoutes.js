3cf91688d9748e10228f7cabf8a4d3123b14e5ec
Kevin Do <do.kevin.vo@gmail.com>
2018-09-26 01:16:59 -0700

    edits erroneous res.joins -> res.json()

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,11 +3,11 @@ const friends = require(`../data/friends`)
 
 module.exports = function(app) {
     app.get(`/api/friends`, function(req, res) {
-        res.join(friends);
+        res.json(friends);
     })
 
     app.post(`/api/friends`, function(req, res) {
         friendArr.push(req.body);
-        res.join(true);
+        res.json(true);
     });
 }
