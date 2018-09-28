f38d777454fd25ca743cd3b89e4831cbaf758375
Kevin Do <do.kevin.vo@gmail.com>
2018-09-27 23:58:52 -0700

    solved match logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,48 +3,48 @@ var friends = [
         "name":"Miami Morty",
         "photo":"https://vignette.wikia.nocookie.net/rickandmorty/images/8/83/PM-150.png/revision/latest?cb=20160923033238",
         "scores":[
-            0,
-            3,
-            2,
             5,
-            0,
+            5,
+            5,
+            1,
+            1,
             3,
-            2,
-            4,
-            4,
-            3
+            1,
+            1,
+            1,
+            5
           ]
     },
     {
         "name":"Hipster Morty",
         "photo":"https://pocketmortys.net/images/assets/MortyHipsterFront.png",
         "scores":[
-            1,
-            1,
-            1,
             5,
             1,
             3,
+            2,
             3,
             1,
+            4,
             1,
-            1
+            3,
+            5
           ]
     },
     {
         "name":"Survivor Morty",
         "photo":"https://vignette.wikia.nocookie.net/rickandmorty/images/3/36/PM-180.png/revision/latest?cb=20170725225540",
         "scores":[
-            2,
-            2,
             1,
-            4,
+            5,
             2,
-            3,
-            4,
-            2,
-            4,
-            2
+            5,
+            1,
+            5,
+            5,
+            5,
+            1,
+            1
           ]
       },
       {
