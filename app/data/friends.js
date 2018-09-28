9a7ca4447e03df3a041947573a64ebae7cfe865d
Kevin Do <do.kevin.vo@gmail.com>
2018-09-28 14:26:58 -0700

    updates db

--- app/data/friends.js
+++ app/data/friends.js
@@ -62,7 +62,39 @@ var friends = [
             3,
             5
           ]
-      }
+      },
+      {
+        "name": "Wizard Morty",
+        "photo": "https://vignette.wikia.nocookie.net/rickandmorty/images/9/99/PM-061.png/revision/latest?cb=20160916025305",
+        "scores": [
+            "5",
+            "4",
+            "4",
+            "1",
+            "1",
+            "3",
+            "4",
+            "2",
+            "1",
+            "3"
+            ]   
+        },
+        {
+        "name": "Scissors-type Morty",
+        "photo": "https://res.cloudinary.com/lmn/image/upload/fl_lossy,q_80/f_auto,w_640,h_360,c_limit/v1/gameskinnyop/5/f/0/5f07e380e5a9f78b4a2150d2a6307583.png",
+        "scores": [
+        "5",
+        "1",
+        "1",
+        "1",
+        "2",
+        "5",
+        "3",
+        "1",
+        "1",
+        "5"
+        ]
+    }
 ];
 
 module.exports = friends;
